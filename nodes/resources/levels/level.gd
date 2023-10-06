e47225502c632bdddd3cd021bcd49251de30358e
sum1nil <fappa@duck.com>
2023-10-06 15:30:16 -0500

    Initial Commit.

---
+++ nodes/resources/levels/level.gd
@@ -0,0 +1,37 @@
+extends Resource
+class_name GameLevel
+
+enum types {
+	wall_top_left,
+	wall_top,
+	wall_top_right,
+	wall_right,
+	wall_bottom_right,
+	wall_bottom,
+	wall_bottom_left,
+	wall_left,
+	empty,
+	player,
+	crate,
+	dot,
+	door,
+}
+
+@export var name := "New Level"
+@export var width := 7
+@export var layout : Array[types] = [
+	types.wall_top_left, types.wall_top, types.wall_top, types.wall_top, 
+types.wall_top_left, types.wall_top, types.wall_top_right,
+	types.wall_left, types.empty, types.empty, types.empty, types.empty,
+types.empty, types.wall_right,
+	types.wall_left, types.empty, types.empty, types.empty, types.empty,
+types.empty, types.wall_right,
+	types.wall_left, types.empty, types.empty, types.empty, types.empty,
+types.empty, types.wall_right,
+	types.wall_left, types.empty, types.empty, types.empty, types.empty,
+types.empty, types.wall_right,
+	types.wall_left, types.empty, types.empty, types.empty, types.empty,
+types.empty, types.wall_right,
+	types.wall_bottom_left, types.wall_bottom, types.wall_bottom, types.wall_bottom,
+types.wall_bottom, types.wall_bottom, types.wall_bottom_right,
+]
